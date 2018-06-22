70235d2cdfdfe754f23480fa2c4fd63f2b3ac2f3
jendralhxr <jendral.hxr@gmail.com>
2018-06-22 10:42:17 +0900

    thickness and local sigmas are updated in the same fashion, p left unused

--- thickness.c
+++ thickness.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <stdlib.h>
 
-double get_thickness(double l, double L, double x, double tau_xy, double wg_1, double wg_2, double wgs_1, double wgs_2, double theta_1, double theta_2, double limit){
+double get_thickness(double l, double L, double x, double tau_xy, double wg_1, double wg_2, double wgs_1, double wgs_2, double theta_1, double theta_2, double limit, double sigma_x_init, double sigma_y_init){
 	double sigma_x, sigma_x_local, sigma_x_local_prev, sigma_x_global;
 	double sigma_y, sigma_y_local, sigma_y_local_prev, sigma_y_global;
 	double mx, my;
@@ -12,10 +12,26 @@ double get_thickness(double l, double L, double x, double tau_xy, double wg_1, d
 	
 	const double E= 200000;
 	const double p= 0.1029;
+	
+	mx= E * pow(thickness,3) / 12 *( \
+			+ wg_1 * (-6/pow(l,2) + 12*x/pow(l,3))
+			+ theta_1 * (4/l -  6*x/pow(l,2))
+			+ wg_2 * (6/pow(l,2)-12*x/pow(l,3))
+			+ theta_2 * (2/l - 6*x/pow(l,2)) );
+	sigma_x_global= 6 * mx  /  pow(thickness,2);
+	my= -E * pow(thickness,3) / 12 *( \
+		+ wgs_1 * (-6/pow(L,2) + 12*x/pow(L,3))
+		+ theta_1 * (4/L -  6*x/pow(L,2))
+		+ wgs_2 * (6/pow(L,2)-12*x/pow(L,3))
+		+ theta_2 * (2/L - 6*x/pow(L,2)) );
+	sigma_y_global= 6 * my / pow(thickness,2);
+	sigma_x_local= sigma_x_init - sigma_x_global;
+	sigma_y_local= sigma_y_init - sigma_y_global;
+		
 	do{
 		thickness_prev= thickness;
 		sigma_x_local_prev= sigma_x_local;
-		sigma_x_local= p*pow(l,2) /E /pow(thickness,2); // kok begini (1)
+		sigma_x_local= pow(thickness_prev,2) /pow(thickness,2) *sigma_x_local_prev;
 		
 		mx= E * pow(thickness,3) / 12 *( \
 			+ wg_1 * (-6/pow(l,2) + 12*x/pow(l,3))
@@ -25,7 +41,7 @@ double get_thickness(double l, double L, double x, double tau_xy, double wg_1, d
 		sigma_x_global= 6 * mx  /  pow(thickness,2);
 	
 		sigma_y_local_prev= sigma_y_local;
-		sigma_y_local= p*pow(l,2) /E /pow(thickness,2); // kok begini? (2)
+		sigma_y_local= pow(thickness_prev,2) /pow(thickness,2) *sigma_y_local_prev;
 		
 		my= -E * pow(thickness,3) / 12 *( \
 			+ wgs_1 * (-6/pow(L,2) + 12*x/pow(L,3))
@@ -52,5 +68,5 @@ double get_thickness(double l, double L, double x, double tau_xy, double wg_1, d
 
 
 int main(int argc, char **argv){
-	printf("%d",get_thickness(20, 30, 40, 0.3, 4.98, 5.1, 4.9, 5.01, 9.69e-4, 3.04e-16, 235);
+	printf("%d",get_thickness(20, 30, 40, 0.3, 4.98, 5.1, 4.9, 5.01, 9.69e-4, 3.04e-16, 235, 20, 20));
 	}
