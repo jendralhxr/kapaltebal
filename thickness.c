6a1482d3bd81aa5d54c27c66f6448b3df6e08860
jendralhxr <jendral.hxr@gmail.com>
2018-06-28 16:32:50 +0900

    two wgs'es

--- thickness.c
+++ thickness.c
@@ -3,28 +3,44 @@
 #include <stdlib.h>
 #include <stdlib.h>
 
-double get_thickness(double l, double L, double x, double tau_xy, double wg_1, double wg_2, double wgs_1, double wgs_2, double theta_1x, double theta_1y, double theta_2x, double theta_2y, double limit, double sigma_x_init, double sigma_y_init){
+// l is span
+// x is ?
+// sigma is stress
+double get_thickness(double l, double x, double dp, double tau_xy, double wg_1, double wg_2, double wgs_1a, double wgs_2a, double wgs_1b, double wgs_2b, double theta_1y, double theta_2y, double theta_1a, double theta_2a, double theta_1b, double theta_2b,  double limit, double sigma_x_init, double sigma_y_init){
 	double sigma_x, sigma_x_local, sigma_x_local_prev, sigma_x_global;
 	double sigma_y, sigma_y_local, sigma_y_local_prev, sigma_y_global;
-	double mx, my;
+	double Mx, My, My1, My2;
 	double sigma_v, sigma_v_prev;
 	double thickness=7, thickness_prev=7, thickness_x, thickness_y;
 	
 	const double E= 200000;
 	const double p= 0.1029;
+	const double L= 143.75;
+	const double y= 71.875;
+	const num_mesh_x= 6;
+	const num_mesh_y= 24;
 	
-	mx= E * pow(thickness,3) / 12 *( \
+	
+	Mx= E * pow(thickness,3) / 12 *( \
 			+ wg_1 * (-6/pow(l,2) + 12*x/pow(l,3))
 			+ theta_1y * (4/l -  6*x/pow(l,2))
 			+ wg_2 * (6/pow(l,2)-12*x/pow(l,3))
 			+ theta_2y * (2/l - 6*x/pow(l,2)) );
-	sigma_x_global= 6 * mx  /  pow(thickness,2);
-	my= -E * pow(thickness,3) / 12 *( \
-		+ wgs_1 * (-6/pow(L,2) + 12*x/pow(L,3))
-		+ theta_1x * (4/L -  6*x/pow(L,2))
-		+ wgs_2 * (6/pow(L,2)-12*x/pow(L,3))
-		+ theta_2x * (2/L - 6*x/pow(L,2)) );
-	sigma_y_global= 6 * my / pow(thickness,2);
+	sigma_x_global= 6 * Mx  /  pow(thickness,2);
+	
+	My1 =  -E * pow(thickness,3) / 12 *( \
+			+ wgs_1a * (-6/pow(L,2) + 12*y/pow(L,3))
+			+ theta_1a * (4/L -  6*y/pow(L,2))
+			+ wgs_2a * (6/pow(L,2)-12*y/pow(L,3))
+			+ theta_2a * (2/L - 6*y/pow(L,2)) );
+	My2 =  -E * pow(thickness,3) / 12 *( \
+			+ wgs_1b * (-6/pow(L,2) + 12*y/pow(L,3))
+			+ theta_1b * (4/L -  6*y/pow(L,2))
+			+ wgs_2b * (6/pow(L,2)-12*y/pow(L,3))
+			+ theta_2b * (2/L - 6*y/pow(L,2)) );
+	My = My1 + ( l / (double)num_mesh_x * (double)dp - 0.0) * (My2 - My1) / l;
+	sigma_y_global= 6 * My / pow(thickness,2);
+	
 	sigma_x_local= sigma_x_init - sigma_x_global;
 	sigma_y_local= sigma_y_init - sigma_y_global;
 		
@@ -33,22 +49,28 @@ double get_thickness(double l, double L, double x, double tau_xy, double wg_1, d
 		sigma_x_local_prev= sigma_x_local;
 		sigma_x_local= pow(thickness_prev,2) /pow(thickness,2) *sigma_x_local_prev;
 		
-		mx= E * pow(thickness,3) / 12 *( \
+		Mx= E * pow(thickness,3) / 12 *( \
 			+ wg_1 * (-6/pow(l,2) + 12*x/pow(l,3))
 			+ theta_1y * (4/l -  6*x/pow(l,2))
 			+ wg_2 * (6/pow(l,2)-12*x/pow(l,3))
 			+ theta_2y * (2/l - 6*x/pow(l,2)) );
-		sigma_x_global= 6 * mx  /  pow(thickness,2);
+		sigma_x_global= 6 * Mx  /  pow(thickness,2);
 	
 		sigma_y_local_prev= sigma_y_local;
 		sigma_y_local= pow(thickness_prev,2) /pow(thickness,2) *sigma_y_local_prev;
 		
-		my= -E * pow(thickness,3) / 12 *( \
-			+ wgs_1 * (-6/pow(L,2) + 12*x/pow(L,3))
-			+ theta_1x * (4/L -  6*x/pow(L,2))
-			+ wgs_2 * (6/pow(L,2)-12*x/pow(L,3))
-			+ theta_2x * (2/L - 6*x/pow(L,2)) );
-		sigma_y_global= 6 * my / pow(thickness,2);
+		My1 =  -E * pow(thickness,3) / 12 *( \
+			+ wgs_1a * (-6/pow(L,2) + 12*y/pow(L,3))
+			+ theta_1a * (4/L -  6*y/pow(L,2))
+			+ wgs_2a * (6/pow(L,2)-12*y/pow(L,3))
+			+ theta_2a * (2/L - 6*y/pow(L,2)) );
+		My2 =  -E * pow(thickness,3) / 12 *( \
+			+ wgs_1b * (-6/pow(L,2) + 12*y/pow(L,3))
+			+ theta_1b * (4/L -  6*y/pow(L,2))
+			+ wgs_2b * (6/pow(L,2)-12*y/pow(L,3))
+			+ theta_2b * (2/L - 6*y/pow(L,2)) );
+		My = My1 + ( l / (double)num_mesh_x * (double)dp - 0.0) * (My2 - My1) / l;
+		sigma_y_global= 6 * My / pow(thickness,2);
 	
 		sigma_x= sigma_x_local + sigma_x_global;
 		sigma_y= sigma_y_local + sigma_y_global;
@@ -66,7 +88,5 @@ double get_thickness(double l, double L, double x, double tau_xy, double wg_1, d
 	return (thickness_prev);
 }
 
-
 int main(int argc, char **argv){
-	printf("%d",get_thickness(20, 30, 40, 0.3, 4.98, 5.1, 4.9, 5.1, 4.9, 5.01, 9.69e-4, 3.04e-16, 235, 20, 20));
 	}
